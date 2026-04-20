0b964c3aec9088592c12136022ef7c99eeefa38f
gudigarrohit <gudigarrohit@gmail.com>
2026-04-21 01:00:46 +0530

    cap

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,39 +3,40 @@
 import Image from "next/image";
 
 const Navbar = () => {
-    return (
-        <nav className="flex justify-between items-center bg-gradient-to-r from-indigo-600 via-purple-600 to-black text-white py-4 md:px-8 px-3 shadow-lg">
+  return (
+    <nav className="flex justify-between items-center bg-gradient-to-r from-indigo-600 via-purple-600 to-black text-white py-4 md:px-8 px-3 shadow-lg">
 
-            {/* Logo */}
-            <div className="logo font-extrabold text-2xl tracking-wide">
-                <span className="cursor-pointer  transition-colors hover:text-violet-300">
-                    iTask Manager
-                </span>
-            </div>
+      {/* Logo */}
+      <div className="logo font-extrabold text-2xl tracking-wide">
+        <span className="cursor-pointer transition-colors hover:text-violet-300">
+          iTask Manager
+        </span>
+      </div>
 
-            {/* Right Side */}
-            <ul className="flex items-center space-x-3 font-medium">
+      {/* Right Side */}
+      <ul className="flex items-center space-x-3 font-medium">
 
-                {/* Avatar */}
-                <li className="cursor-pointer">
-                    <div className="relative w-10 h-10 rounded-full overflow-hidden border-2 border-white hover:scale-105 transition">
-                        <Image
-                            src="/avatar.jpeg"
-                            alt="User Avatar"
-                            fill
-                            className="object-cover"
-                        />
-                    </div>
-                </li>
+        {/* Avatar */}
+        <li className="cursor-pointer">
+          <div className="relative w-10 h-10 rounded-full overflow-hidden border-2 border-white hover:scale-105 transition">
+            <Image
+              src="https://res.cloudinary.com/dyvpe5aio/image/upload/w_200,h_200,c_fill/v1776712852/wallpaper_kzm12i.jpg"
+              alt="User Avatar"
+              fill
+              sizes="40px"
+              className="object-cover"
+            />
+          </div>
+        </li>
 
-                {/* My Task */}
-                <li className="cursor-pointer relative group font-bold  transition-colors">
-                    My Task
-                </li>
+        {/* My Task */}
+        <li className="cursor-pointer relative group font-bold">
+          My Task
+        </li>
 
-            </ul>
-        </nav>
-    );
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
